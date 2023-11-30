e798fc0ba2f41ec146da64d2823bba76a308cb74
huangyuanhui <2278181701@qq.com>
2023-11-30 20:01:38 +0800

    spring cloud alibaba的nacos注册中心 使用RestTemplate实现服务的远程调用！ 为什么还可以使用RestTemplate实现服务的远程调用？因为nacos知识实现了注册中心的接口，服务调用的实现完全可以用回RestTemplate!

--- order-service/src/main/java/com/cloud/order/OrderApplication.java
+++ order-service/src/main/java/com/cloud/order/OrderApplication.java
@@ -3,11 +3,25 @@ package com.cloud.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @MapperScan("com.cloud.order.mapper")
 @SpringBootApplication
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+
+    /**
+     * 使用RestTemplate实现服务远程调用
+     * @return
+     */
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- order-service/src/main/java/com/cloud/order/controller/OrderController.java
+++ order-service/src/main/java/com/cloud/order/controller/OrderController.java
@@ -1,9 +1,14 @@
 package com.cloud.order.controller;
 
+import com.cloud.order.pojo.Order;
+import com.cloud.order.pojo.User;
 import com.cloud.order.service.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/orders")
@@ -12,5 +17,15 @@ public class OrderController {
     @Autowired
     private IOrderService orderService;
 
+    @Autowired
+    private RestTemplate restTemplate;
 
+    @GetMapping("/{id}")
+    public Order getById(@PathVariable("id") Long id) {
+        Order order = orderService.getById(id);
+        User user =
+                restTemplate.getForObject("http://user-service/users/" + order.getUserId(), User.class);
+        order.setUser(user);
+        return order;
+    }
 }
