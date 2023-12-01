15afac4bae9d84428ceb1d637704f8f58d3982a4
huangyuanhui <2278181701@qq.com>
2023-12-01 17:40:48 +0800

    nacos配置管理中心 以及 nacos文档

---
+++ user-service/src/main/java/com/cloud/user/config/PatternProperties.java
@@ -0,0 +1,16 @@
+package com.cloud.user.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "pattern")
+public class PatternProperties {
+    private String dateformat;
+
+    private String envSharedValue;
+
+    private String name;
+}
--- user-service/src/main/java/com/cloud/user/controller/UserController.java
+++ user-service/src/main/java/com/cloud/user/controller/UserController.java
@@ -1,22 +1,45 @@
 package com.cloud.user.controller;
 
+import com.cloud.user.config.PatternProperties;
 import com.cloud.user.pojo.User;
 import com.cloud.user.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 @RequestMapping("/users")
+//@RefreshScope
 public class UserController {
 
     @Autowired
     private IUserService userService;
 
+    //    @Value("${pattern.dateformat}")
+//    private String dateformat;
+    @Autowired
+    private PatternProperties properties;
+
     @GetMapping("/{id}")
     public User getById(@PathVariable("id") Long id) {
         return userService.getById(id);
+    }
+
+    @GetMapping("/prop")
+    public PatternProperties properties() {
+        return properties;
+    }
+
+    @GetMapping("/now")
+    public String now() {
+        return LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern(properties.getDateformat()));
     }
 }
